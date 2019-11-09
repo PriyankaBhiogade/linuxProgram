650fde57b6ec2072de3eaaef538e8325fffe9bec
PriyankaBhiogade <bhiogadepriyanka93@gmail.com>
2019-11-09 15:42:10 +0530

    whileLoopProgarm

--- whileLoop.sh
+++ whileLoop.sh
@@ -1,13 +1,26 @@
-#!/bin/bash -x
+isPartTime=2;
+ratePerHrs=100;
+monthlySalary=0;
 valid=true
-count=1;
+count=1
+totalHrs=0
 while [ $valid ]
-do
-	echo $count
-	if [ $count -eq 3 ]
-	then 
-		break;
-	else
-		((count++))
-	fi
+do 
+    if [ $totalHrs -le 50 ]
+    then   
+	empCheck=$(( RANDOM % 3))
+    		 case $empCheck in
+        	     $isFullTime)
+          	          empHrs=8;;
+        	     $isPartTime )
+          	          empHrs=4;;
+        	     * )
+         	          empHrs=0;;
+                 esac  
+	
+       $totalHrs=$(( $empHrs + $totalHrs ))
+  else
+   monthlySalary=$(( $perDaySalary + $totalHrs *))
+    break;
+  fi
 done
