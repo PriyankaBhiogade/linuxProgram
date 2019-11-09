81cd4584ec1d8993e02acefcaee2f191af7a7765
PriyankaBhiogade <bhiogadepriyanka93@gmail.com>
2019-11-09 16:01:52 +0530

    whileLoop

--- whileLoop.sh
+++ whileLoop.sh
@@ -1,12 +1,12 @@
+#!/bin/bash -x
 isPartTime=2;
 ratePerHrs=100;
 monthlySalary=0;
 valid=true
-count=1
 totalHrs=0
 while [ $valid ]
 do 
-    if [ $totalHrs -le 50 ]
+    if [ $totalHrs -le 10 ]
     then   
 	empCheck=$(( RANDOM % 3))
     		 case $empCheck in
@@ -17,10 +17,14 @@ do
         	     * )
          	          empHrs=0;;
                  esac  
-	
+		
        $totalHrs=$(( $empHrs + $totalHrs ))
+	
   else
-   monthlySalary=$(( $perDaySalary + $totalHrs *))
+    # $totalHrs=$(( $empHrs + $totalHrs ))
+
+   #monthlySalary=$(( $perDaySalary + $totalHrs *))
     break;
+    
   fi
 done
