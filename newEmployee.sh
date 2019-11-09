259ef9b7cde3f49401ae34e294d7bcee8a67b933
vasamvishal <vasamvishal12@gmail.com>
2019-11-09 17:42:05 +0530

    added part time changes

--- newEmployee.sh
+++ newEmployee.sh
@@ -3,12 +3,15 @@ isFullTime=1;
 isPartTime=2;
 rate=100
 empCheck=$(( RANDOM % 2))
-if [ $empCheck == 1 ]
-then
-workingHrs=8;
-salary=rate*workingHrs;
-else
-workingHrs=0;
-salary=rate*workingHrs;
-fi
+	if [ $empCheck == isFullTime ]
+	then
+		workingHrs=8;
+		salary=$rate*$workingHrs;
+	elif [ $empCheck == isPartTime ]
+		workingHrs=4;
+		salary=$rate*$workingHrs;
+	else 
+		workingHrs=0;
+		salary=$rate*$workingHrs;
+	fi
 
