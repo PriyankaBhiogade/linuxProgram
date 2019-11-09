feaebf2d4dd3e130651304b8adc1ddc2f84746f7
vasamvishal <vasamvishal12@gmail.com>
2019-11-09 18:07:51 +0530

    refactored empcheck tp randomcheck

--- newEmployee.sh
+++ newEmployee.sh
@@ -2,13 +2,13 @@
 isFullTime=1;
 isPartTime=2;
 rate=100
-empCheck=$(( RANDOM % 2))
-if [ $empCheck == 1 ]
-then
-workingHrs=8;
-salary=rate*workingHrs;
-else
-workingHrs=0;
-salary=rate*workingHrs;
-fi
+randomCheck=$(( RANDOM % 2))
+	if [ $randomCheck == 1 ]
+	then
+		workingHrs=8;
+		salary=$(( $rate*$workingHrs ));
+	else
+		workingHrs=0;
+		salary=$(( $rate*$workingHrs ));
+	fi
 
