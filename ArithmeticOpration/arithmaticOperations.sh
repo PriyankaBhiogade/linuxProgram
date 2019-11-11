349882b76be49fd3606ba5d88cc9950ca5eed8dc
vasamvishal <vasamvishal12@gmail.com>
2019-11-11 16:31:03 +0530

    ArithmeticOpration taking input from user

---
+++ ArithmeticOpration/arithmaticOperations.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -x
+
+	read -p "Enter First Value: " x
+	read -p "Enter Second Value: " y
+	read -p "Enter Third Value: " z
+	 	echo "x:" $x
+		echo "y:" $y
+		echo "z:" $z
+
--- newEmployee.sh
+++ newEmployee.sh
@@ -1,43 +1,28 @@
-#!/bin/bash -x
 
-isFullTime=1;
-isPartTime=2;
-ratePerHrs=100;
- function totalHrsFunction() {
-	empCheck=$(( RANDOM % 3))
-   case $empCheck in
-        $isFullTime)
-          empHrs=8;;
-        $isPartTime )
-          empHrs=4;;
- 
-       *)
-         empHrs=0;;
-   esac  
-   totalHrs=$(perDaySalaryFunction $((empHrs)))
-	echo $totalHrs
-  }
- 
- function perDaySalaryFunction() {
-	#for (( counter=1; counter<=20; counter++))
-	#do
-    		perDaySalary=$(( $1 * $ratePerHrs ))
-	 echo $perDaySalary
-	#done
- }
-for (( counter=1; counter<=20; counter++))
-do
-  
- totalResult=$(totalHrsFunction) 
-	totalSalary[((counter))]=$totalResult
-monthlySalary=0;
- echo $monthlySalary
- for elem in ${totalSalary[@]}; do
-	echo $elem
-    monthlySalary=$(( $monthlySalary + $elem ))
-done
-done
-   echo $monthlySalary
+#!/bin/bash -x 
+isFullTime=1; 
+isPartTime=2; 
+empRatePerHr=20;
+ workingDays=20; 
+count=0; 
+totalWorkingHours=0; 
+totalWorkingDays=0; 
+declare -A Wages 
+function dailyHour() {
+ case $1 in 
+$isFullTime) 
+hour=8;; 
+$isPartTime) 
+hour=4;; *) hour=0;; esac echo $hour } 
+function wages() { TotalWages=$(( $1 * $empRatePerHr )) echo $TotalWages } 
+for (( counter=1; counter<=20; counter++ )) do workHour="$( dailyHour $(( RANDOM%3 )) )" #totalWorkingHours=$(( $totalWorkingHours + $workH$
+echo ${!Wages[@]} echo ${Wages[@]} echo ${#Wages[@]} echo "Total Salary" ${Wages[TotalSalary]}
+
+
+
+
+
+
 
 
 
