5c8cc5db89bfc65718aaba37254a32681e216b3b
vasamvishal <vasamvishal12@gmail.com>
2019-11-11 15:36:45 +0530

    find sum from array

--- newEmployee.sh
+++ newEmployee.sh
@@ -1,12 +1,10 @@
 #!/bin/bash -x
+
 isFullTime=1;
 isPartTime=2;
 ratePerHrs=100;
-monthlySalary=0;
- function myfunction() {
-      echo "total number of working hrs" $1;
-  } 
-  empCheck=$(( RANDOM % 3))
+ function totalHrsFunction() {
+	empCheck=$(( RANDOM % 3))
    case $empCheck in
         $isFullTime)
           empHrs=8;;
@@ -16,11 +14,34 @@ monthlySalary=0;
        *)
          empHrs=0;;
    esac  
-
-  result="$( myfunction $(( $empHrs)))"
-  
-
+   totalHrs=$(perDaySalaryFunction $((empHrs)))
+	echo $totalHrs
+  }
  
+ function perDaySalaryFunction() {
+	#for (( counter=1; counter<=20; counter++))
+	#do
+    		perDaySalary=$(( $1 * $ratePerHrs ))
+	 echo $perDaySalary
+	#done
+ }
+for (( counter=1; counter<=20; counter++))
+do
+  
+ totalResult=$(totalHrsFunction) 
+	totalSalary[((counter))]=$totalResult
+monthlySalary=0;
+ echo $monthlySalary
+ for elem in ${totalSalary[@]}; do
+	echo $elem
+    monthlySalary=$(( $monthlySalary + $elem ))
+done
+done
+   echo $monthlySalary
+
+
+
+
 
 
 
